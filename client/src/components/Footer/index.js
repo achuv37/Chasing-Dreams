2d2b6336448c0f7fd4f5699311362b1c99f5ab16
Mikekuney <96913841+Mikekuney@users.noreply.github.com>
2022-07-10 19:30:39 -0400

    added server folder and updated package.json

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+
+// Footer component
+function Footer() {
+  return (
+    <footer className="footer">
+      <div>
+        <a href='http://facebook.com/' target='_blank'>
+          <img 
+            src={require("../../assets/logos/facebook.jpg")}
+            alt="Facebook"
+            className="logos"
+          ></img>
+        </a>
+      </div>
+      
+      <div>
+        <a href='https://www.linkedin.com/' target='_blank'>
+          <img 
+            src={require("../../assets/logos/linkedin.png")}
+            alt="linkedin"
+            className="logos"
+          ></img>
+        </a>
+      </div>
+      <div>
+        <a href='https://www.twitter.com/' target='_blank'>
+          <img 
+            src={require("../../assets/logos/twitter.png")}
+            alt="twitter"
+            className="logos"
+          ></img>
+        </a>
+      </div>
+    </footer>
+  );
+  
+}
+
+export default Footer;
---
+++ client/src/components/Login/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h1>You can login here</h1>
+    </div>
+  )
+}
+
+export default Login;
