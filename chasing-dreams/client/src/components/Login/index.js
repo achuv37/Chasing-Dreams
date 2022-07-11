a18cf590b39645b736c8dade23037bba4dc843c0
Achu Ajesh <achuv37@gmail.com>
2022-07-10 20:54:55 -0400

    Add login-form

--- chasing-dreams/client/src/components/Login/index.js
+++ chasing-dreams/client/src/components/Login/index.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+
+
+
 
 function Login() {
   return (
-    <div>
-      <h1>You can login here</h1>
-    </div>
+    <main className='flex-row justify-center mb-4'>
+      <div className='col-12 col-md-6'>
+        <div className='card'>
+          <h4 className='card-header'>Login</h4>
+          <div className='card-body'>
+            <form>
+              <input
+                className='form-input'
+                placeholder='Your email'
+                name='email'
+                type='email'
+                id='email'
+                
+              />
+              <input
+                className='form-input'
+                placeholder='******'
+                name='password'
+                type='password'
+                id='password'
+                
+              />
+              <button className='btn d-block w-100' type='submit'>
+                Submit
+              </button>
+            </form>
+          
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
